3b05f27f3cf744d5d7164523eb5a6985393a6a98
DigaoF <83978141+DigaoF@users.noreply.github.com>
2021-07-19 08:58:31 -0300

    todo completo, rascunho pc pixel

---
+++ pixelsArt/script.js
@@ -0,0 +1,6 @@
+const pixelBoard = document.getElementById('pixel-board');
+const cores = document.getElementsByClassName('color');
+const paleta = document.getElementById('color-palette');
+const limpaButao = document.getElementById('clear-board');
+const geraBoard = document.getElementById('generate-board');
+var corPadrao = 'rgb(0,0,0)';
